d7b8fdf26bf1ceb0919a932a8663921708703243
Tairy <tairyguo@gmail.com>
2014-06-28 13:06:43 +0800

    实现顶部下拉搜索页面

---
+++ js/project.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('#search').click(function(){
+    $('#search_page').slideDown('slow');
+  });
+  $('#search_page_btn').click(function(){
+    $('#search_page').slideUp('slow');
+  });
+});
